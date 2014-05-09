264b39fa0039a67d3a294826fce2ae489788d9bb
Allen Zhao <cnallenzhao@gmail.com>
2014-05-09 17:20:14 +0800

    Lesson 1 - Mini Harp Static Server
    Implement for Lesson 1 - Mini Harp Static Server, Serving Static
    Content.

--- mini-harp/bin/mini-harp.js
+++ mini-harp/bin/mini-harp.js
@@ -1,14 +1,8 @@
 #!/usr/bin/env node
-var createMiniHarp = require("mini-harp")
-  , app = createMiniHarp();
+var createMiniHarp = require("mini-harp");
 var parseArgs = require("minimist")(process.argv.slice(2));
+var root = parseArgs._[0] || process.cwd();
 var port = parseArgs.port || 4000;
 console.log("Starting mini-harp on http://localhost:"+port);
+var app = createMiniHarp(root);
 app.listen(port);
-app.use(function(request, response, next){
-	if(request.url=="/current-time"){
-		response.end((new Date()).toISOString());
-	}else{
-		next();
-	}
-});
--- mini-harp/index.js
+++ mini-harp/index.js
@@ -1,4 +1,6 @@
-module.exports = function createMiniHarp () {
+module.exports = function createMiniHarp (root) {
         var app = require("connect")();
+        var serveStatic = require("serve-static");
+        app.use(serveStatic(root));
         return app;
 }
