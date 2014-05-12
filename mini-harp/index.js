99fa3d2b36225b351b2460b1930c371644cc83f2
Allen Zhao <cnallenzhao@gmail.com>
2014-05-12 20:23:16 +0800

    Lesson 2 - Middleware rewrite
    rewrite the middleware to implement dropStupidRequest made.

--- mini-harp/index.js
+++ mini-harp/index.js
@@ -3,18 +3,17 @@ module.exports = function createMiniHarp(root) {
     var serveStatic = require("serve-static");
     var makeJade = require("./lib/processor/jade.js");
     var makeLess = require("./lib/processor/less.js");
+    var dropStupidRequset = require('./lib/middleware/dropStupidRequset.js')();
     var path = require("path");
     app.use(function(request, response, next) {
         var extname = path.extname(request.url);
         if (request.url == "/current-time") {
             response.end((new Date()).toISOString());
-        } else if (extname == '.less' || extname == '.jade') {
-            response.statusCode = 404;
-            response.end("404 Not Found.");
         } else {
             next();
         }
     });
+    app.use(dropStupidRequset);
     app.use(serveStatic(root));
     app.use(makeJade(root));
     app.use(makeLess(root));
---
+++ mini-harp/lib/middleware/dropStupidRequset.js
@@ -0,0 +1,14 @@
+module.exports = dropStupidRequest;
+
+function dropStupidRequest() {
+    var path = require('path');
+    return function(request, response, next) {
+        var extname = path.ext(request.url);
+        if (extname == '.jade' || extname = '.less') {
+            response.statusCode(404);
+            response.end('ERROR:404 NOT FOUND. PLEASE TRY AGAIN');
+        } else {
+            next();
+        }
+    }
+}
